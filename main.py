7f2f9c2e836ea9d3b4a2b4f9904ce046c559fb3a
Stephen-sanchez1 <stephen.sanchez@stonybrook.edu>
2023-05-12 17:39:43 +0000

    new version

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
-from fastapi import FastAPI
-import pandas as pd
-import uvicorn
+from flask import Flask, request 
+import pandas as pd 
 
 df = pd.read_csv('./data/diagnoses2019.csv')
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/")
+@app.route('/', methods=["GET"])
 def home():
-    return {"This is a API service for MN ICD code details."}
+    return 'this is a API service for MN ICD code details'
 
-@app.get("/preview")
+@app.route('/preview', methods=["GET"])
 def preview():
-    top10rows = df.head(10)
+    top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
     return result
-    
-@app.get("/svc/{value}")
-def svccode(value):
-    print("value: ", value)
-    filtered = df[df["svc_code"] == value]
+
+@app.route('/icd/<value>', methods=['GET'])
+def icdcode(value):
+    print('value: ', value)
+    filtered = df[df['principal_diagnosis_code'] == value]
     if len(filtered) <= 0:
-        return {"There is nothing here"}
+        return 'There is nothing here'
     else: 
         return filtered.to_json(orient="records")
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.route('/icd/<value>/sex/<value2>')
+def icdcode2(value, value2):
+    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered2 = filtered[filtered['sex'] == value2]
+    if len(filtered2) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered2.to_json(orient="records")    
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
